e14e2d327ff99a68d0dc3b72f21922c12c7c4420
Ibrahim Bocus <ibrahim.bocus@sigtech.com>
2022-06-24 14:32:09 +0100

    removed unneccesary define parameters call

--- submissionscript/submission.py
+++ submissionscript/submission.py
@@ -3,20 +3,6 @@ import sys
 import os
 import mysql.connector
 
-# - A database upgrade requires the execution of numbered SQL scripts stored in a specified folder, named such as `045.createtable.sql`
-#   - Sample scripts are provided in the `dbscripts` directory
-# - The scripts may contain any simple SQL statement(s) to any table of your choice, e.g. `INSERT INTO testTable VALUES("045.createtable.sql");`
-# - There may be gaps in the SQL file name numbering and there isn't always a . (dot) after the beginning number
-# - The database upgrade is based on looking up the current version in the database and comparing this number to the numbers in the script names
-# - The table where the current db version is stored is called `versionTable`, with a single row for the version, called `version`
-# - If the version number from the db matches the highest number from the scripts then nothing is executed
-# - All scripts that contain a number higher than the current db version will be executed against the database in numerical order
-# - In addition, the database version table is updated after the script execution with the executed script's number
-# - Your script will be executed automatically via a program, and must satisfy these command line input parameters exactly in order to run:
-#   - `./db-upgrade.your-lang directory-with-sql-scripts username-for-the-db db-host db-name db-password`
-#   - Example (bash): `./db-upgrade.sh dbscripts myUser myDbServer techTestDB SuperSecretPassword1!`
-
-
 def tryint(s):
     try:
         return int(s)
@@ -103,18 +89,16 @@ def update_version(username, host, database, password, version):
 
 # order scripts, then execute them
 def execute_scripts(directory, username, host, database, password):
-    parameters = define_parameters()
-
     files = order_scripts(directory)
     files = ignore_older_versions(files, current_version(username, host, database, password))
-    
+
     print(f"Executing scripts: {files}")
     for file in files:
         sql = read_script(file, directory)
         execute_sql(sql, username, host, database, password)
         latest_version = alphanum_key(file)[1]
         print(f"latest version: {latest_version}")
-        update_version(parameters['username'], parameters['host'], parameters['database'], parameters['password'], latest_version)
+        update_version(username, host, database, password, latest_version)
 
 def main():
     parameters = define_parameters()
